5e840d8a0394a9194b66c9c644e1243f06827df2
riptheir <49995327+riptheir@users.noreply.github.com>
2019-08-17 05:57:10 -0400

    Update ghuc.go

--- github-username-checker/ghuc.go
+++ github-username-checker/ghuc.go
@@ -3,27 +3,72 @@ package main
 import (
 	"bufio"
 	"flag"
-	"log"
+	"fmt"
+	"github.com/gookit/color"
 	"net/http"
 	"os"
 )
 
 func main() {
-	u := flag.String("u", "u.txt", "list of usernames")
+	// elite ascii art is a prerequisite for hacking properly
+	black := color.FgBlack.Render
+	blue := color.FgBlue.Render
+	fmt.Printf("%s%s\n", black("https://github.com/rip/go-ghoul/"), blue("github-username-checker"))
+	color.Magenta.Println("                        _                _ ")
+	color.Magenta.Println("  __ _  ___  ___  __ _ | |_   ___  _  _ | |")
+	color.Magenta.Println(" / _` |/ _ \\|___|/ _` || ' \\ / _ \\| || || |")
+	color.Magenta.Println(" \\__, |\\___/     \\__, ||_||_|\\___/ \\_,_||_|")
+	color.Magenta.Println(" |___/           |___/\u001b[0m")
+	// user input
+	usernamesPath := flag.String("u", "", "path to file containing usernames")
+	threads := flag.Int("t", 99, "amount of simultaneous checking threads")
+	verbose := flag.Bool("v", false, "verbose")
 	flag.Parse()
-	f, err := os.Open(*u)
-	if err != nil {
-		log.Fatal(err)
+	// validate input
+	if *usernamesPath == "" || *threads < 1 {
+		color.Red.Println("-h for help")
+		os.Exit(1)
 	}
+	// initiate janky golang threadpooling
+	semaphore := make(chan bool, *threads)
+	// read file
+	f, err := os.Open(*usernamesPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// populate slices with requisite data
 	s := bufio.NewScanner(f)
-	for s.Scan() {
-		r, err := http.Get("https://github.com/" + s.Text())
-		if err != nil {
-			log.Println(err)
-		}
-		defer r.Body.Close()
-		if r.StatusCode == 404 {
-			log.Println(s.Text())
-		}
+	// for slice in slices
+	for s.Scan() { // todo turn this into a slice to remove successfully checked and iterate over until all are checked because of request/proxy errors
+		username := s.Text()
+		// mark thread "in use"
+		semaphore <- true
+		// invoke reave with a goroutine
+		go func(username string) {
+			// mark thread available after anonymous function has completed
+			defer func() { <-semaphore }()
+			// do stuff
+			r, err := http.Get("https://github.com/" + username)
+			if err != nil {
+				color.Yellow.Println(err)
+			} else { // don't panic on errors
+				// friendly reminder to close stuff
+				r.Close = true
+				defer r.Body.Close()
+				// psst... person reading this, ur a nerd 🤓
+				switch r.StatusCode {
+				case 404:
+					color.Green.Println(username)
+				default:
+					if *verbose {
+						color.Red.Println(username)
+					}
+				}
+			}
+		}(username) // arg... removing this seems to just skip first one?
+	}
+	// clean up thread pool on loop completion
+	for i := 0; i < cap(semaphore); i++ {
+		semaphore <- true
 	}
 }
